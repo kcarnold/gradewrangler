0226a99693ff690055ece45d678af610ef7a44ec
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2021-12-17 17:43:57 -0800

    JS wrangling code

---
+++ inst/js/scrape_moodle_manual_grading.js
@@ -0,0 +1,22 @@
+responses = [];
+$('.essay').each(function(idx) {
+  let container = $(this);
+  let parent = container.prevAll('h4').first().text();
+	let match = /Attempt number (\d+) for (.+)$/.exec(parent);
+  let attempt = +match[1], name = match[2];
+  let essay = (
+    container.find('textarea.qtype_essay_response').val()
+    || container.find('div.qtype_essay_response').text()
+    );
+  responses.push({name, attempt, essay});
+});
+copy(JSON.stringify(responses))
+
+/*
+headerMatch = document.querySelector("[role=main] > h3").textContent.match(/Grading question (\d+): (.+)/);
+copy(JSON.stringify({
+  questionNum: +headerMatch[1],
+  questionTitle: headerMatch[2],
+  responses
+}))
+*/
